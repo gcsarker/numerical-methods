e61f933a17c8a9bc07fe4ff91468d17a90ee2f41
Gobinda Chandra Sarker <104276753+gcsarker@users.noreply.github.com>
2022-04-25 00:23:32 +0600

    Delete Utility Functions directory

--- Utility Functions/data_table.m
+++
@@ -1,6 +0,0 @@
-function data_table(data,col_names)
-    tab = array2table(data,'VariableNames',col_names);
-    fprintf('\n\n');
-    disp(tab);
-end
-
--- Utility Functions/relative_error.m
+++
@@ -1,3 +0,0 @@
-function r = relative_error(x_approx,x_exact)
-    r = (abs(x_exact-x_approx)/x_exact);
-end
