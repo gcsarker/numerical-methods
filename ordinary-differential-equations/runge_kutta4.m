fbe4c805b1d1e40a6f4dbf9371179573ba89860c
Gobinda Chandra Sarker <104276753+gcsarker@users.noreply.github.com>
2022-04-25 20:39:19 +0600

    Update runge_kutta4.m

--- ordinary-differential-equations/runge_kutta4.m
+++ ordinary-differential-equations/runge_kutta4.m
@@ -4,6 +4,8 @@ function Y = runge_kutta4(ode,x0,y0,h,xn)
     X = zeros(n,1);
     Y = zeros(n,1);
 
+    col_names = {'xn','Yn'};
+    tab_rows = [];
     X(1) = x0;
     Y(1) = y0;
     for i=1:n
@@ -23,7 +25,10 @@ function Y = runge_kutta4(ode,x0,y0,h,xn)
         yf = subs(xf,y,Y(i)+k3*h);
         k4 = double(yf);
         
+        tab_rows(end+1,:) = [X(i) Y(i)];
         Y(i+1) = Y(i)+(k1+2*k2+2*k3+k4)*(h/6);
         X(i+1) = x0 + i*h;
     end
+    tab_rows(end+1,:) = [X(n+1) Y(n+1)];
+    data_table(tab_rows,col_names);
 end
